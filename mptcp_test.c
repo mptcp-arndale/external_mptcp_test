e759bd6a66c5aae631bbcb1a097b088b50bb19dc
Martin Heistermann <mh-git@sponc.de>
2013-12-07 19:56:45 +0900

    init

---
+++ mptcp_test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <netinet/tcp.h>
+
+int main(int argc, char *argv[])
+{
+	printf("TODO: implement test\n");
+#ifdef TCP_MUTIPATH_CONNID
+	printf("TCP_MULTIPATH_CONNID is known: %u\n", TCP_MULTIPATH_CONNID);
+#else
+	printf("TCP_MULTIPATH_CONNID unknown\n");
+#endif
+	return 0;
+}
