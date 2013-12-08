2a3dd0968510331b009951ffb226dbb7a39e19c4
Martin Heistermann <mh-git@sponc.de>
2013-12-08 17:24:19 +0900

    Implement proper test tool for getsockopt(TCP_MULTIPATH_CONNID).

--- mptcp_test.c
+++ mptcp_test.c
@@ -1,13 +1,107 @@
+/*
+ * based on http://beej.us/guide/bgnet/examples/client.c
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <netinet/tcp.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+
+
+#define MAXDATASIZE 100 // max number of bytes we can get at once 
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 int main(int argc, char *argv[])
 {
-	printf("TODO: implement test\n");
-#ifdef TCP_MULTIPATH_CONNID
-	printf("TCP_MULTIPATH_CONNID is known: %u\n", TCP_MULTIPATH_CONNID);
-#else
-	printf("TCP_MULTIPATH_CONNID unknown\n");
-#endif
+	int sockfd, numbytes;  
+	char buf[MAXDATASIZE];
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s[INET6_ADDRSTRLEN];
+
+	if (argc != 3) {
+	    fprintf(stderr,"usage: mptcp_test <hostname> <port>\n");
+	    exit(1);
+	}
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and connect to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("client: socket");
+			continue;
+		}
+
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("client: connect");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "client: failed to connect\n");
+		return 2;
+	}
+
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+			s, sizeof s);
+	printf("client: connected to %s\n", s);
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	uint32_t optval = 0;
+	socklen_t optlen = 23;
+	int res = getsockopt(sockfd, SOL_TCP, TCP_MULTIPATH_CONNID, &optval, &optlen);
+	printf("optlen = %u\n", optlen);
+	if (res != 0) {
+		perror("getsockopt(..., TCP_MULTIPATH_CONNID,...)");
+		close(sockfd);
+		return 3;
+	}
+	if (optlen != sizeof(optval))
+		return 4;
+	printf("connid = %x\n", optval);
+
+	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+	    perror("recv");
+	    exit(1);
+	}
+
+	buf[numbytes] = '\0';
+
+	printf("client: received '%s'\n",buf);
+
+	close(sockfd);
+
 	return 0;
 }
+
